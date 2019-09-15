1508c638575af81400645abcba3e81d3ec32ba3b
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2019-09-15 15:37:27 +0300

    change travis test command with default test env

---
+++ projects/web/src/environments/environment.test.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  envName: 'TEST',
+  test: true,
+  i18nPrefix: '',
+  appName: 'ngx-jsonresume',
+  url: 'https://ngx-jsonresume.firebaseapp.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAwRrxjjft7KMdhwfLKPkd8PCBR3JFaLfo',
+    authDomain: 'angularfirestore.firebaseapp.com',
+    databaseURL: 'https://angularfirestore.firebaseio.com',
+    projectId: 'angularfirestore',
+    storageBucket: 'angularfirestore.appspot.com',
+    messagingSenderId: '1039984584356',
+  },
+};
