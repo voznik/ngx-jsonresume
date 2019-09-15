9ac82dcc1dd096a5af0938ac6b3669a9f01a8593
Vadym Parakonnyi <v.parakonnyi@gmail.com>
2019-09-15 23:27:48 +0300

    fix functions ssr

--- projects/server/webpack.server.config.js
+++ projects/server/webpack.server.config.js
@@ -7,24 +7,40 @@ const webpack = require('webpack');
 const SRC_PATH = path.join(process.cwd(), 'projects/web/src');
 const OUT_PATH = path.join(__dirname, '../../dist/web-server');
 const ENTRY_PATH = path.join(process.cwd(), 'projects/server/src/server.ts');
+// https://github.com/firebase/firebase-js-sdk/issues/1797
+const regex = /^firebase(\/([\w\d]+))*/;
 
 module.exports = {
   mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
-    server: ENTRY_PATH
+    server: ENTRY_PATH,
   },
   target: 'node',
-  resolve: { extensions: ['.ts', '.js'] },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  // https://github.com/firebase/firebase-js-sdk/issues/1455#issuecomment-455712500
+  // this makes sure we include node_modules and other 3rd party libraries
+  externals: [
+    /node_modules/,
+    function(context, request, callback) {
+      // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+      if (regex.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    },
+  ],
   optimization: {
-    minimize: false
+    minimize: false,
   },
   output: {
     // Puts the output at the root of the dist folder
     path: OUT_PATH,
     library: 'app',
     libraryTarget: 'umd',
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     noParse: /polyfills-.*\.js/,
@@ -34,9 +50,9 @@ module.exports = {
         // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
         // Removing this will cause deprecation warnings to appear.
         test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
-        parser: { system: true }
-      }
-    ]
+        parser: { system: true },
+      },
+    ],
   },
   plugins: [
     new webpack.ContextReplacementPlugin(
@@ -50,6 +66,6 @@ module.exports = {
       /(.+)?express(\\|\/)(.+)?/,
       path.join(__dirname, SRC_PATH),
       {}
-    )
-  ]
+    ),
+  ],
 };
